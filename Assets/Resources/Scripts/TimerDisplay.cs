8c9a624ea02523bc8e5ac6a1e1ebea1d33b47c67
SteamPoweredAnimal <dot@steamfox.net>
2016-02-28 14:44:58 +0100

    .

--- Assets/Resources/Scripts/TimerDisplay.cs
+++ Assets/Resources/Scripts/TimerDisplay.cs
@@ -6,11 +6,15 @@ public class TimerDisplay : MonoBehaviour {
 
 	public static TimerDisplay Me;
 
+	public int Min = 20, Max = 60;
+	public float Ratio = 4f;
+
 	void Awake() {
 		Me = this;
 	}
 
 	public void Refresh(float dt) {
 		GetComponent<Text>().text = ((int)dt).ToString();
+		GetComponent<Text> ().fontSize = (int) (20f + (10f - dt) * 4);
 	}
 }
