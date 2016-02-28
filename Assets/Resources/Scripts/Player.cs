99b772764a48e363a67ad3209223dc2974bd9ff6
Philon <therealphilon@gmail.com>
2016-02-28 16:10:53 +0100

    ekran startowy, muzyczka startowa. nowy dzwiek smierci

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -20,17 +20,21 @@ public class Player : MonoBehaviour {
 	}
 
 	public void KillMe() {
-		Game.Me.PanelLives.ReduceLive();
-		Game.Me.CreateTileAt(transform.localPosition.x, transform.localPosition.y, new Tile(TileType.DeadMan, Rotation.Up), Game.Me.MissionContainer.transform);
-		//Instantiate (DyingManPrefab, transform.position + new Vector3(-1.25f, -1.25f, 0), Quaternion.identity);
-		Instantiate (SkullPrefab, transform.position, Quaternion.identity);
-		Game.Me.SpawnPlayer();
-		PlayerManager.Me.RestartTimer();
+		if (Game.Me != null) {
+			Game.Me.PanelLives.ReduceLive();
+			Game.Me.CreateTileAt(transform.localPosition.x, transform.localPosition.y, new Tile(TileType.DeadMan, Rotation.Up), Game.Me.MissionContainer.transform);
+			//Instantiate (DyingManPrefab, transform.position + new Vector3(-1.25f, -1.25f, 0), Quaternion.identity);
+			Game.Me.SpawnPlayer();
+			PlayerManager.Me.RestartTimer();
+		}
+		Instantiate(SkullPrefab, transform.position, Quaternion.identity);
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 
-		Debug.Log(string.Format("{0} triggered with {1} ", gameObject.name, other.gameObject.name));
+		if (other.GetComponent<TileComponent>() == null) {
+			return;
+		}
 
 		Tile otherTile = other.GetComponent<TileComponent>().Tile;
 		if (otherTile.Type == TileType.Spikes) {
